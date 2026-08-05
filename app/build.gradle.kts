3a59d9e88c7a154ff24ffb13817de890b1d1233a
Danijel Jovanović <danijel.xda@gmail.com>
2026-08-05 11:53:17 +0200

    enable real Compose backdrop blur

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.haze)
+    implementation(libs.haze.materials)
 
     // AndroidX
     implementation(libs.androidx.core.ktx)
