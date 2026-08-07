0e25062d716b3b4fff9d25b638a94d8e62291c8e
Danijel Jovanović <danijel.xda@gmail.com>
2026-08-07 07:05:04 +0200

    Raise bottom navigation dock

--- app/src/main/java/com/owlcoder/animeschedule/presentation/navigation/AnimeBottomBar.kt
+++ app/src/main/java/com/owlcoder/animeschedule/presentation/navigation/AnimeBottomBar.kt
@@ -120,7 +120,7 @@ fun AnimeBottomBar(
         modifier = Modifier
             .fillMaxWidth()
             .windowInsetsPadding(WindowInsets.navigationBars.only(WindowInsetsSides.Bottom))
-            .padding(start = 13.dp, end = 13.dp, top = 6.dp, bottom = 10.dp),
+            .padding(start = 13.dp, end = 13.dp, top = 6.dp, bottom = 14.dp),
     ) {
         Box(
             modifier = Modifier
